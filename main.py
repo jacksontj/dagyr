710af66bcdb838fe9d7a03ac293873c380797c70
Thomas Jackson <jacksontj.89@gmail.com>
2016-07-28 08:26:22 -0700

    Autoreload on config change (for easier development)

--- main.py
+++ main.py
@@ -1,15 +1,39 @@
 import yaml
+import functools
+import os
 
 import logging
 logging.basicConfig(level=logging.INFO)
+log = logging.getLogger(__name__)
 
 import tornado.web
+import tornado.ioloop
 
 import dag_proxy.handler
 import dag_proxy.dag
 
 
+def _reload_config_on_update(app, filepath, modify_times):
+    try:
+        modified = os.stat(filepath)
+    except Exception:
+        return
+
+    if filepath not in modify_times or modified != modify_times[filepath]:
+        log.debug("config change, reloading config")
+        app.dag_config = _load_config(config_file)
+        modify_times[filepath] = modified
+
+
+def _load_config(filepath):
+    with open('config.yaml', 'r') as fh:
+        cfg = yaml.load(fh)
+        return dag_proxy.dag.DagConfig(cfg)
+
+
 if __name__ == "__main__":
+    ioloop = tornado.ioloop.IOLoop.current()
+
 
     # only a single handler-- that does all the DAG magic
     app = tornado.web.Application(
@@ -17,12 +41,19 @@ if __name__ == "__main__":
         debug=True,
     )
 
-    # TODO: out of band
-    # load config file
-    with open('config.yaml', 'r') as fh:
-        cfg = yaml.load(fh)
-        app.dag_config = dag_proxy.dag.DagConfig(cfg)
+    # TODO: config? autoreload is only nice for development and debugging
+    config_file = 'config.yaml'
+    modify_times = {}
+    scheduler = tornado.ioloop.PeriodicCallback(
+        functools.partial(_reload_config_on_update, app, config_file, modify_times),
+        500,  # how often to call this
+        io_loop=ioloop,
+    )
+    scheduler.start()
+
+    # load initial config file
+    app.dag_config = _load_config(config_file)
 
 
     app.listen(8888)
-    tornado.ioloop.IOLoop.current().start()
+    ioloop.start()

e64f7705e11c03bd3b2691f18a79d8df0d44fcf1
Thomas Jackson <jacksontj.89@gmail.com>
2016-07-29 09:02:54 -0700

    Some initial usage of the "option_data"
    After doing this, it does bring up a few questions (noted in TODO), for now I'll leave it so I have something to iterate with

--- dag_proxy/fragments/attribute_match.py
+++ dag_proxy/fragments/attribute_match.py
@@ -8,4 +8,11 @@ def fragment(ctx, frag_spec, frag_args):
     '''
     # TODO: global cache of converted datastructures (trie etc.)
     # check if the item we want is in the value list
-    return ctx.getattr_dotted(frag_args['attribute']) in frag_args['values']
+
+    # TODO: this expansion really belongs up a few layers-- since it only needs
+    # to happen once, and ideally we'd resolve all these before marking the
+    # config as good-- as it may not link/resolve
+    if frag_spec['values'].get('option_data'):
+        return ctx.getattr_dotted(frag_args['attribute']) in ctx.options[frag_args['values']]
+    else:
+        return ctx.getattr_dotted(frag_args['attribute']) in frag_args['values']

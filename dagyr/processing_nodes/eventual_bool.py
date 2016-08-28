2cb9cf8fe2647fab810073a49ecac62d89f1f841
Thomas Jackson <jacksontj.89@gmail.com>
2016-08-27 17:39:58 -0700

    Merge in the useful eventual_bool implementation from one of the original iteration implementation branches

--- dagyr/processing_nodes/eventual_bool.py
+++ dagyr/processing_nodes/eventual_bool.py
@@ -1,20 +1,20 @@
-'''Be false until called a few times
+'''Change values after a certain number of calls
+
+This node will return `starting_value` until it has been called `change_count` times
+at which point it will start returning `eventual_value`. The counter storing how
+many times this node has been called will be stored in `counter_location`
+
 '''
 
-KEY = 'state.ebool_count'
-
-# TODO: get args to this!
 def processing_node(context, arg_spec, raw_args, resolved_args):
-    count = 0
+    count = raw_args['starting_value']
     try:
-        count = context.getattr_dotted(KEY)
+        count = context.getattr_dotted(raw_args['counter_location'])
     except:
         pass
-    if count < 3:
+    if count < raw_args['change_count']:
         count += 1
-        context.setattr_dotted(KEY, count)
-        print 'false', count
-        return False
+        context.setattr_dotted(raw_args['counter_location'], count)
+        return raw_args['starting_value']
     else:
-        print 'true', count
-        return True
+        return raw_args['eventual_value']
